6b053c2ea94f617d31fa59869dbe98068541d67d
shadman-a <ahmedshadman966@gmail.com>
2021-06-21 12:58:24 -0400

    communication fixed

--- force-app/main/default/lwc/productCard/productCard.js
+++ force-app/main/default/lwc/productCard/productCard.js
@@ -1,6 +1,9 @@
 import { LightningElement, track } from 'lwc';
 import getProducts from '@salesforce/apex/ProductService.getProducts';
 import addToCart from '@salesforce/apex/ProductService.addToCart';
+import Id from '@salesforce/user/Id';
+// import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
 
 
 export default class ProductCard extends LightningElement {
@@ -11,7 +14,7 @@ export default class ProductCard extends LightningElement {
         console.log(result);
         this.listOfProducts = result;
         this.error = undefined;
-        const isAddedToCart = false
+        const isAddedToCart = true
         const quantity = 1
       })
 
@@ -19,12 +22,21 @@ export default class ProductCard extends LightningElement {
         this.error = error;
         this.listOfProducts = undefined;
       });
-
   }
 
-  handleAddToCart(){
-    console.log("hello")
-    
+  handleAddToCart(event){
+    console.log("product id",event.target.dataset.id)
+    console.log("user id",Id)
+    addToCart({ product: event.target.dataset.id, currentUser: Id })
+    .then((result) => {
+      console.log(result);
+    })
+
+    .catch((error) => {
+      this.error = error;
+      console.log(error)
+    });
+
   }
 
 
