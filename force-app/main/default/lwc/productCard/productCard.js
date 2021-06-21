0d8c877424ba9f6b0e55865b4bfb514037ed819f
shadman-a <ahmedshadman966@gmail.com>
2021-06-21 18:22:48 -0400

    cart persisting

--- force-app/main/default/lwc/productCard/productCard.js
+++ force-app/main/default/lwc/productCard/productCard.js
@@ -1,6 +1,7 @@
 import { LightningElement, track } from 'lwc';
 import getProducts from '@salesforce/apex/ProductService.getProducts';
 import addToCart from '@salesforce/apex/ProductService.addToCart';
+import createCart from '@salesforce/apex/ProductService.createCart';
 import Id from '@salesforce/user/Id';
 // import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
@@ -8,6 +9,7 @@ import Id from '@salesforce/user/Id';
 
 export default class ProductCard extends LightningElement {
   @track listOfProducts;
+  cartId;
   connectedCallback() {
     getProducts()
       .then((result) => {
@@ -17,17 +19,29 @@ export default class ProductCard extends LightningElement {
         const isAddedToCart = true
         const quantity = 1
       })
-
       .catch((error) => {
         this.error = error;
         this.listOfProducts = undefined;
       });
+
+      createCart({ currentUser: Id })
+      .then((result) => {
+        this.cartId = result
+       
+      })
+      .catch((error) => {
+        this.error = error;
+      });
+
+    
+
+    
   }
 
   handleAddToCart(event){
     console.log("product id",event.target.dataset.id)
     console.log("user id",Id)
-    addToCart({ product: event.target.dataset.id, currentUser: Id })
+    addToCart({ product: event.target.dataset.id, currentUser: Id, cartId: this.cartId })
     .then((result) => {
       console.log(result);
     })
